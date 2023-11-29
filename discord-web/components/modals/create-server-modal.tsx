51df362c6471b607fe0f4c2910c5c15b19278fd4
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-29 22:55:20 +0700

    update create server modal

--- discord-web/components/modals/create-server-modal.tsx
+++ discord-web/components/modals/create-server-modal.tsx
@@ -24,8 +24,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { useModal } from "@/hooks/use-modal-store";
 import FileUpload from "../file-upload";
+import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
   name: z.string().min(1, {
