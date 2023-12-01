c4ec2b04f3b486410bff63179808d8493326de02
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-12-01 15:05:40 +0700

    create server member

--- discord-web/components/server/server-member.tsx
+++ discord-web/components/server/server-member.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { cn } from '@/lib/utils';
 import { Member, MemberRole, Profile } from '@prisma/client'
-import { Server } from 'http';
 import { ShieldAlert, ShieldCheck } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react'
@@ -22,8 +21,13 @@ const ServerMember = ({member,server}:ServerMemberProps) => {
     const router=useRouter();
     const params=useParams();
     const icon=roleIconMap[member.role];
+    const onClick=()=>{
+      router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+    }
   return (
-    <button className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",params?.memberId===member.id&&"bg-zinc-700/20 dark:bg-zinc-700")}>
+    <button
+      onClick={onClick}
+     className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",params?.memberId===member.id&&"bg-zinc-700/20 dark:bg-zinc-700")}>
     <UserAvatar src={member.profile.imageUrl} className='h-8 w-8 md:h-8 md:w-8'/>
     <p
         className={cn(
