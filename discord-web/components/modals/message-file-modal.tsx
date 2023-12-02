f20f79e95473e1aafca0a37e62e5ceda0e0c7670
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-12-03 00:35:14 +0700

    create message file modal

--- discord-web/components/modals/message-file-modal.tsx
+++ discord-web/components/modals/message-file-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import {
@@ -11,49 +11,50 @@ import {
   DialogHeader,
   DialogTitle,
 } from "../ui/dialog";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
-import { Input } from "../ui/input";
+import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Button } from "../ui/button";
 import FileUpload from "../file-upload";
-import axios from "axios"
+import axios from "axios";
 import { useRouter } from "next/navigation";
+import qs from "query-string";
 import { useModal } from "@/hooks/use-modal-store";
 const formSchema = z.object({
-  name: z.string().min(1, {
-    message: "Server name is required",
-  }),
-  imageUrl: z.string().min(1, {
-    message: "Server Image is required",
+  fileUrl: z.string().min(1, {
+    message: "Attachment is required",
   }),
 });
 const MessageFileModal = () => {
-    const {isOpen,onClose,type,data}=useModal()
-    const router=useRouter()
-    
-    const isModalOpen=isOpen&&type==="messageFile";
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+  const { apiUrl, query } = data;
+  const isModalOpen = isOpen && type === "messageFile";
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
-      imageUrl: "",
+      fileUrl: "",
     },
   });
-  const handleClose=()=>{
+  const handleClose = () => {
     form.reset();
     onClose();
-  }
-  
+  };
+
   const isLoading = form.formState.isSubmitting;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/servers",values);
+      const url = qs.stringifyUrl({
+        url: apiUrl || "",
+        query,
+      });
+      await axios.post(url, {
+        ...values,
+        content: values.fileUrl,
+      });
       form.reset();
       router.refresh();
-      window.location.reload()
-      
+      handleClose();
     } catch (error) {
       console.log(error);
-      
     }
   };
   return (
@@ -61,11 +62,10 @@ const MessageFileModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl font-bold text-center">
-            Customize your server
+            Add an attachment
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and an image. You can
-            always change it later.
+            Send a file as a message
           </DialogDescription>
         </DialogHeader>
         {/* Form */}
@@ -74,48 +74,28 @@ const MessageFileModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-               <FormField
-                    control={form.control}
-                    name="imageUrl"
-                    render={({field})=>(
-                        <FormItem>
-                            <FormControl>
-                                <FileUpload
-                                    endpoint="serverImage"
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                />
-                            </FormControl>
-                        </FormItem>
-                    )}  
-               />
-
-               
+                <FormField
+                  control={form.control}
+                  // name phai trung voi defaultt value
+                  name="fileUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="messageFile"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
-
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">Server Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={isLoading}
-                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        placeholder="Enter server name"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
             </div>
             <DialogFooter className="bg--gray-100 px-6 py-4">
-                    <Button variant="primary" disabled={isLoading}>
-                        Create
-                    </Button>
+              <Button variant="primary" disabled={isLoading}>
+                Send
+              </Button>
             </DialogFooter>
           </form>
         </Form>
