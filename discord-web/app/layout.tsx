02b35fbdfd23bbe03f1f88f7acb6dbda5d1748ec
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-12-02 11:43:05 +0700

    configuration for socket

--- discord-web/app/layout.tsx
+++ discord-web/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { SocketProvider } from '@/components/providers/socket-provider'
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -21,8 +22,11 @@ export default function RootLayout({
        <html lang="en" suppressHydrationWarning>
         <body className={font.className}>
          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem storageKey='discord-theme'>
-          <ModalProvider/>
-          {children}
+          <SocketProvider>
+
+            <ModalProvider/>
+            {children}
+          </SocketProvider>
          </ThemeProvider>
         </body>
       </html>
