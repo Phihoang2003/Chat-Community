667c46c158a6cd32c289871ad53ea0eca9aa026a
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-12-04 09:59:34 +0700

    update chat message

--- discord-web/components/chat/chat-messages.tsx
+++ discord-web/components/chat/chat-messages.tsx
@@ -1,19 +1,20 @@
-import { Member } from '@prisma/client'
-import React from 'react'
-import ChatWelcome from './chat-welcome';
+'use client'
+import { Member } from "@prisma/client";
+import React from "react";
+import ChatWelcome from "./chat-welcome";
+import useChatQuery from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
 
-interface ChatMessagesProps{
-  name:string,
-  member:Member;
-  chatId:string,
-  apiUrl:string;
-  socketUrl:string;
-  socketQuery:Record<string,string>
-  paramKey:"channelId"|"conversationId"
-  paramValue:string,
-  type:"channel"|"conversation"
-  
-
+interface ChatMessagesProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
 }
 const ChatMessages = ({
   name,
@@ -24,19 +25,49 @@ const ChatMessages = ({
   socketQuery,
   paramKey,
   paramValue,
-  type
-}:ChatMessagesProps) => {
+  type,
+}: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useChatQuery({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+  });
+  // if (status === "pending") {
+  //   return (
+  //     <div className="flex flex-col flex-1 justify-center items-center">
+  //       <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+  //       <p className="text-xs text-zinc-500 dark:text-zinc-400">
+  //         Loading messages...
+  //       </p>
+  //     </div>
+  //   )
+  // }
+
+  // if (status === "error") {
+  //   return (
+  //     <div className="flex flex-col flex-1 justify-center items-center">
+  //       <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+  //       <p className="text-xs text-zinc-500 dark:text-zinc-400">
+  //         Something went wrong!
+  //       </p>
+  //     </div>
+  //   )
+  // }
   return (
     // flex-1 se tu dong tang kich thuoc va co dan lai cho phu hop theo chieu cao cua phan tu cha
-    <div className='flex-1 flex flex-col overflow-y-auto py-4 '>
-        <div className='flex-1'/>
-        <ChatWelcome
-          type={type}
-          name={name}
-        />
+    <div className="flex-1 flex flex-col overflow-y-auto py-4 ">
+      <div className="flex-1" />
+      <ChatWelcome type={type} name={name} />
     </div>
-  )
-}
+  );
+};
 
-
-export default ChatMessages
+export default ChatMessages;
