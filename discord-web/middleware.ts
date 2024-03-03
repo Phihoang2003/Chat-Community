5be7cc0e3cdb39e258687f7f010a1a0adbc65de9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-03-03 18:56:40 +0700

    update code

--- discord-web/middleware.ts
+++ discord-web/middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -6,8 +7,56 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing","/api/socket/io", '/api/webhook/clerk'],
   ignoredRoutes: ['/api/webhook/clerk'],
+
+  async afterAuth(auth, req) {
+        if (auth.isPublicRoute) {
+          //  For public routes, we don't need to do anything
+          return NextResponse.next();
+        }
+    
+        const url = new URL(req.nextUrl.origin);
+    
+        if (!auth.userId) {
+          //  If user tries to access a private route without being authenticated,
+          //  redirect them to the sign in page
+          url.pathname = "/sign-in";
+          return NextResponse.redirect(url);
+    
+        }
+      },
 });
  
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// export default authMiddleware({
+//   publicRoutes: [
+//     "/",
+//     "/sign-in(.*)",
+//     "/sign-up(.*)",
+//     "/api(.*)",
+//     "/api/uploadthing",
+//   ],
+  
+//   async afterAuth(auth, req) {
+//     if (auth.isPublicRoute) {
+//       //  For public routes, we don't need to do anything
+//       return NextResponse.next();
+//     }
+
+//     const url = new URL(req.nextUrl.origin);
+
+//     if (!auth.userId) {
+//       //  If user tries to access a private route without being authenticated,
+//       //  redirect them to the sign in page
+//       url.pathname = "/sign-in";
+//       return NextResponse.redirect(url);
+
+//     }
+//   },
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)"],
+// };
